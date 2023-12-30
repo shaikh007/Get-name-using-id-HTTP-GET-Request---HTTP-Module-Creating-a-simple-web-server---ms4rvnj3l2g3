da4daa7c21430dc5b0db61d5c0e9664cd6ccea9a
Shaikh Irfan <shaikhji1998@gmail.com>
2023-12-30 20:40:26 +0530

    new commit

--- src/app.js
+++ src/app.js
@@ -12,6 +12,29 @@ app.use(express.json())
 
 // GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
+app.get('/api/v1/names/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+
+    const product = productNames.find((item) => item.id === productId);
+
+    if (product) {
+        res.status(200).json({
+            status: 'success',
+            message: 'Product name fetched successfully',
+            data: {
+                name: {
+                    id: product.id,
+                    name: product.name
+                }
+            }
+        });
+    } else {
+        res.status(404).json({
+            status: 'failure',
+            message: 'Not found!'
+        });
+    }
+});
 
 
 module.exports = app;
